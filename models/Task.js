37898d7720c245fd7cedd598fb6c8e7773c9840e
Jakub Was <jakubwas.jw@gmail.com>
2020-06-27 00:11:57 +0200

    Create auth

---
+++ models/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  priority: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("task", TaskSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -30,7 +32,7 @@ router.post(
       let user = await User.findOne({ email: email });
 
       if (user) {
-        return res.status.status(400).json({ msg: "User already exists" });
+        return res.status(400).json({ msg: "User already exists" });
       }
 
       user = new User({
@@ -46,7 +48,24 @@ router.post(
       // Save user to database
       await user.save();
 
-      res.send("User saved");
+      // Create & send token (jwt)
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
