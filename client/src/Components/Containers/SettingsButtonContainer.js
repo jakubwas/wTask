e4bf89b6bb31a693c3bccc51bc689a0a3eee2413
Jakub Was <jakubwas.jw@gmail.com>
2020-06-28 03:44:11 +0200

    Add settings buttons

---
+++ client/src/Components/Containers/SettingsButtonContainer.js
@@ -0,0 +1,23 @@
+// Dependencies
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 3rem;
+  padding: 0.5rem;
+  font-size: 1.6rem;
+  display: flex;
+  cursor: pointer;
+
+  &:hover {
+    background: #316fea;
+    color: white;
+  }
+`;
+
+const SettingsButtonContainer = (props) => {
+  return <Container>{props.children}</Container>;
+};
+
+export default SettingsButtonContainer;
--- client/src/Components/Layout/SettingsDropDown.js
+++ client/src/Components/Layout/SettingsDropDown.js
@@ -1,6 +1,14 @@
 // Dependencies
 import React from "react";
 import styled from "styled-components";
+// React Components
+import SettingsButtonContainer from "../Containers/SettingsButtonContainer";
+
+// React Components
+import FeaturePreviewButton from "../Buttons/SettingsButtons/FeaturePreviewButton";
+import SupportButton from "../Buttons/SettingsButtons/SupportButton";
+import SettingsButton from "../Buttons/SettingsButtons/SettingsButton";
+import LogoutButton from "../Buttons/SettingsButtons/LogoutButton";
 
 const Container = styled.div`
   position: fixed;
@@ -11,10 +19,21 @@ const Container = styled.div`
   background: white;
   box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.4);
   border-radius: 7px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
-const SettingsDropDown = (props) => {
-  return <Container>{props.children}</Container>;
+const SettingsDropDown = () => {
+  return (
+    <Container>
+      <FeaturePreviewButton>What's new ?</FeaturePreviewButton>
+      <SupportButton>Support</SupportButton>
+      <SettingsButton>Settings</SettingsButton>
+      <LogoutButton>Logout</LogoutButton>
+    </Container>
+  );
 };
 
 export default SettingsDropDown;
