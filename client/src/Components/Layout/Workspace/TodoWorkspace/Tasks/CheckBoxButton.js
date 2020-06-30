1c2b06f78b55a094bd2a7ee9f80507fd9b4b632e
Jakub Was <jakubwas.jw@gmail.com>
2020-06-30 03:51:14 +0200

    Change task color depending on priority (completed)

--- client/src/Components/Layout/Workspace/TodoWorkspace/Tasks/CheckBoxButton.js
+++ client/src/Components/Layout/Workspace/TodoWorkspace/Tasks/CheckBoxButton.js
@@ -37,13 +37,17 @@ const CheckBoxButton = ({ id, priority, status }) => {
       case 0:
         if (status === "uncompleted") {
           setColorPalette({
-            border: "1px solid grey",
+            border: "1px solid rgb(128,128,128)",
             bgColor: "rgb(255, 255, 255, 1)",
             bgHover: "rgba(0, 0, 0, 0.1)",
             fill: "rgba(0, 0, 0, 0.5)",
           });
         } else {
-          setColorPalette({});
+          setColorPalette({
+            border: "none",
+            bgColor: "rgb(128,128,128)",
+            fill: "rgb(255, 255, 255)",
+          });
         }
         break;
       case 1:
@@ -55,7 +59,11 @@ const CheckBoxButton = ({ id, priority, status }) => {
             fill: "rgba(0, 0, 255, 0.8)",
           });
         } else {
-          setColorPalette({});
+          setColorPalette({
+            border: "none",
+            bgColor: "rgba(0, 0, 255, 0.8)",
+            fill: "rgb(255,255,255)",
+          });
         }
         break;
       case 2:
@@ -67,7 +75,11 @@ const CheckBoxButton = ({ id, priority, status }) => {
             fill: "rgba(219, 147, 64, 0.8)",
           });
         } else {
-          setColorPalette({});
+          setColorPalette({
+            border: "none",
+            bgColor: "rgba(219, 147, 64, 0.8)",
+            fill: "rgb(255, 255, 255)",
+          });
         }
         break;
       case 3:
@@ -79,7 +91,11 @@ const CheckBoxButton = ({ id, priority, status }) => {
             fill: "rgba(255, 0, 0, 0.8)",
           });
         } else {
-          setColorPalette({});
+          setColorPalette({
+            border: "none",
+            bgColor: "rgba(255, 0, 0, 0.8)",
+            fill: "rgb(255, 255, 255)",
+          });
         }
         break;
 
@@ -89,35 +105,62 @@ const CheckBoxButton = ({ id, priority, status }) => {
   }, []);
 
   return (
-    <Container
-      border={colorPallete.border}
-      bgColor={colorPallete.bgColor}
-      bgHover={colorPallete.bgHover}
-      onMouseEnter={() => {
-        setSvgDisplay("block");
-      }}
-      onMouseLeave={() => {
-        setSvgDisplay("none");
-      }}
-      onClick={() => {
-        console.log("aaaa");
-        toggleStatus(id);
-      }}
-    >
-      <Svg
-        x="0px"
-        y="0px"
-        viewBox="0 0 512 512"
-        style={{ display: `${svgDsiaply}` }}
-        fill={colorPallete.fill}
-      >
-        <path
-          d="M504.502,75.496c-9.997-9.998-26.205-9.998-36.204,0L161.594,382.203L43.702,264.311c-9.997-9.998-26.205-9.997-36.204,0
+    <Fragment>
+      {status === "uncompleted" ? (
+        <Container
+          border={colorPallete.border}
+          bgColor={colorPallete.bgColor}
+          bgHover={colorPallete.bgHover}
+          onMouseEnter={() => {
+            setSvgDisplay("block");
+          }}
+          onMouseLeave={() => {
+            setSvgDisplay("none");
+          }}
+          onClick={() => {
+            console.log("aaaa");
+            toggleStatus(id);
+          }}
+        >
+          <Svg
+            x="0px"
+            y="0px"
+            viewBox="0 0 512 512"
+            style={{ display: `${svgDsiaply}` }}
+            fill={colorPallete.fill}
+          >
+            <path
+              d="M504.502,75.496c-9.997-9.998-26.205-9.998-36.204,0L161.594,382.203L43.702,264.311c-9.997-9.998-26.205-9.997-36.204,0
 			c-9.998,9.997-9.998,26.205,0,36.203l135.994,135.992c9.994,9.997,26.214,9.99,36.204,0L504.502,111.7
 			C514.5,101.703,514.499,85.494,504.502,75.496z"
-        />
-      </Svg>
-    </Container>
+            />
+          </Svg>
+        </Container>
+      ) : (
+        <Container
+          border={colorPallete.border}
+          bgColor={colorPallete.bgColor}
+          bgHover={colorPallete.bgHover}
+          onClick={() => {
+            toggleStatus(id);
+          }}
+        >
+          <Svg
+            x="0px"
+            y="0px"
+            viewBox="0 0 512 512"
+            style={{ display: "inlineBlock" }}
+            fill={colorPallete.fill}
+          >
+            <path
+              d="M504.502,75.496c-9.997-9.998-26.205-9.998-36.204,0L161.594,382.203L43.702,264.311c-9.997-9.998-26.205-9.997-36.204,0
+    c-9.998,9.997-9.998,26.205,0,36.203l135.994,135.992c9.994,9.997,26.214,9.99,36.204,0L504.502,111.7
+    C514.5,101.703,514.499,85.494,504.502,75.496z"
+            />
+          </Svg>
+        </Container>
+      )}
+    </Fragment>
   );
 };
 
