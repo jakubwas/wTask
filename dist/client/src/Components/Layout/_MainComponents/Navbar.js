d80aa5409484e7fbf1043dab82cec6585b437477
Jakub Was <jakubwas.jw@gmail.com>
2020-07-18 01:44:32 +0200

    Show current user info on navbar

--- dist/client/src/Components/Layout/_MainComponents/Navbar.js
+++ dist/client/src/Components/Layout/_MainComponents/Navbar.js
@@ -1,5 +1,5 @@
 // Dependencies
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import styled from "styled-components";
 // React-Components
 import BurgerMenu from "../Navbar/BurgerMenu";
@@ -18,12 +18,50 @@ const NavigationBar = styled.div`
   top: 0;
   width: 100vw;
 `;
+const AppLogo = styled.h1`
+  margin-left: 1rem;
+  font-size: 2rem;
+  color: rgba(255, 255, 255, 0.8);
+`;
+const RightIconsContainer = styled.div`
+  display: flex;
+`;
+const UserInfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Info = styled.h3`
+  font-size: 1.1rem;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 0.4);
+  font-style: italic;
+  margin-right: 0.8rem;
+`;
+const Email = styled.h2`
+  font-size: 1.1rem;
+  font-style: italic;
+  letter-spacing: 0.1rem;
+  color: rgba(255, 255, 255, 0.8);
+`;
 // React Component
 const Navbar = () => {
+  const authContext = useContext(AuthContext);
+  const { user } = authContext;
   return (
     <NavigationBar>
-      <BurgerMenu />
-      <Settings />
+      <AppLogo>WTask</AppLogo>
+      <RightIconsContainer>
+        {user && (
+          <UserInfoContainer>
+            <Info>Logged in as </Info>
+            <Email>{user.email}</Email>
+          </UserInfoContainer>
+        )}
+        <BurgerMenu />
+        <Settings />
+      </RightIconsContainer>
     </NavigationBar>
   );
 };
