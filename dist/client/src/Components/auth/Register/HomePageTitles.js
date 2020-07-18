b9a0442840dd4dcc2a64d0a384d2b3a919ef1e80
Jakub Was <jakubwas.jw@gmail.com>
2020-07-18 04:32:24 +0200

    Finish register page style

--- dist/client/src/Components/auth/Register/HomePageTitles.js
+++ dist/client/src/Components/auth/Register/HomePageTitles.js
@@ -28,6 +28,17 @@ const HomePageSubtitle = styled.h2`
     font-size: 1.4rem;
   }
 `;
+const HomePageSubText = styled.h3`
+  color: white;
+  font-family: "Poppins", sans-serif;
+  font-weight: 300;
+  font-style: italic;
+  font-size: 1.3rem;
+  @media screen and (max-width: 1200px) {
+    margin-top: 0.5rem;
+    font-size: 1.1rem;
+  }
+`;
 // React-Component
 const HomePageTitles = () => {
   return (
@@ -36,6 +47,10 @@ const HomePageTitles = () => {
       <HomePageSubtitle>
         with the best productivity app - WTask
       </HomePageSubtitle>
+      <HomePageSubText>
+        WTask helps you take control of your tasks and saves you time by
+        bringing them all into your preferred task manager.
+      </HomePageSubText>
     </TitlesContainer>
   );
 };
